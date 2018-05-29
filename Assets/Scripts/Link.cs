678217a447323c42c2d7ccae69f80b568f2c01a8
Omar Garcia <omarcruz11@hotmail.com>
2018-05-29 19:33:26 +0200

    Merge

--- Assets/Scripts/Link.cs
+++ Assets/Scripts/Link.cs
@@ -19,13 +19,23 @@ public class Link : MonoBehaviour {
 
     public void DrawLink(Vector3 startPos, Vector3 endPos)
     {
+        Node a = m_board.FindNodeAt(Utility.Vector3Round(startPos));
+
+        Vector3 vectorUp = new Vector3(0, 1, 0);
+        if (Utility.Vector3Round(a.transform.forward) == new Vector3(0, -1, 0))
+            vectorUp = new Vector3(0, 0, 1);
+        else if (Utility.Vector3Round(a.transform.forward) == new Vector3(0, 0, -1))
+            vectorUp = new Vector3(1, 0, 0);
+        else if (Utility.Vector3Round(a.transform.forward) == new Vector3(0, 0, 1))
+            vectorUp = new Vector3(0, 1, 0);
+
         linkObject = gameObject;
         transform.localScale = new Vector3(lineThickness, 1f, 0f);
         Vector3 dirVector = endPos - startPos;
         //dirVector = new Vector3(dirVector.x, dirVector.y, dirVector.z - 0.2f);
         float zScale = dirVector.magnitude - borderWidth * 2f;
         Vector3 newScale = new Vector3(lineThickness, 1f, zScale);
-        transform.rotation = Quaternion.LookRotation(dirVector);
+        transform.rotation = Quaternion.LookRotation(dirVector, vectorUp);
         if (dirVector.x == 0f && dirVector.z == 0)
             transform.rotation *= Quaternion.AngleAxis(180, Vector3.forward);
         transform.position = startPos + (transform.forward * borderWidth);
